6c850a7bc01b4a245c99aa607703bb5a1a630be4
Felix <felix@ciiro.dev>
2026-07-09 15:10:13 +0100

    update index.php

--- index.php
+++ index.php
@@ -1,67 +1,87 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <meta http-equiv="cache-control" content="max-age=0" />
-  <meta http-equiv="cache-control" content="no-cache" />
-  <meta http-equiv="expires" content="0" />
-  <meta http-equiv="pragma" content="no-cache" />
-  <link rel="stylesheet" href="/css/bootstrap.min.css" />
-  <link rel="stylesheet" href="/css/fontawesome.min.css" />
-  <link rel="stylesheet" href="/css/brands.min.css" />
-  <link rel="stylesheet" href="/css/solid.min.css" />
-  <link rel="stylesheet" href="/css/latin.css" />
-  <link rel="stylesheet" href="/css/styles.css" />
-  <link rel="icon" href="/img/favicon.ico" type="image/x-icon" />
-  <title>ciiro.dev</title>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+	<meta http-equiv="cache-control" content="max-age=0" />
+	<meta http-equiv="cache-control" content="no-cache" />
+	<meta http-equiv="expires" content="0" />
+	<meta http-equiv="pragma" content="no-cache" />
+	<link rel="stylesheet" href="/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="/css/fontawesome.min.css" />
+	<link rel="stylesheet" href="/css/brands.min.css" />
+	<link rel="stylesheet" href="/css/solid.min.css" />
+	<link rel="stylesheet" href="/css/latin.css" />
+	<link rel="stylesheet" href="/css/styles.css" />
+	<link rel="icon" href="/img/favicon.ico" type="image/x-icon" />
+	<title>ciiro.dev</title>
 </head>
 
 <body class="h-100 pt-3 d-flex text-center">
-  <div class="container h-100">
-    <div class="row h-100 align-items-center">
-      <div class="col">
-        <header>
-          <img src="/img/github.png" alt="Felix Steer" class="rounded portrait">
-          <div class="title">
-            <h1 class="mt-3">Felix Steer</h1>
-            <a class="small" href="https://en.pronouns.page/@ciiro" target="_blank"
-              aria-label="Pronouns Page">(they/them)</a>
-          </div>
-          <h2 class="lead fs-4">Developer and IT Administrator</h2>
-        </header>
-        <div class="content-separator"></div>
-        <main class="lead">
-          <p class="fs-5">
-            <a class="discrete-link pe-2" href="https://github.com/FriendlyUser1" target="_blank"
-              data-bs-toggle="tooltip" aria-label="GitHub" data-bs-original-title="GitHub"><i
-                class="fa-brands fa-github"></i></a>
-            <a class="discrete-link pe-2" href="https://bsky.app/profile/ciiro.dev" target="_blank"
-              data-bs-toggle="tooltip" aria-label="Bluesky" data-bs-original-title="Bluesky"><i
-                class="fa-brands fa-bluesky"></i>
-            </a>
-          <div class="content-separator"></div>
-          <p class="fs-5">
-            <i class="fa-solid fa-envelope"></i>
-            <a href="mailto:felix@ciiro.dev" class="discrete-link">felix@ciiro.dev</a>
-            <br>
-            <i class="fa-solid fa-key"></i>
-            <a href="data/Felix_Steer.asc" class="discrete-link">PGP Key</a>
-          </p>
-          </p>
-        </main>
-        <footer class="py-3">
-          <div class="pt-3">by Felix at <a href="https://ciiro.dev">ciiro.dev</a></div>
-        </footer>
-      </div>
-    </div>
-  </div>
-  <script src="/js/bootstrap.bundle.js"></script>
-  <script>
-    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-    const tooltipList = tooltipTriggerList.map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl))
-  </script>
+	<div class="container h-100">
+		<div class="row h-100 align-items-center">
+			<div class="col">
+				<header>
+					<img
+						src="/img/github.png"
+						alt="Felix Steer"
+						class="rounded portrait" />
+					<div class="title">
+						<h1 class="mt-3">Felix Steer</h1>
+						<a
+							class="small"
+							href="https://en.pronouns.page/@ciiro"
+							target="_blank"
+							aria-label="Pronouns Page">(they/them)</a>
+					</div>
+					<h2 class="lead fs-4">Developer and IT Administrator</h2>
+				</header>
+				<div class="content-separator"></div>
+				<main class="lead">
+					<p class="fs-5">
+						<a
+							class="discrete-link pe-2"
+							href="https://github.com/FriendlyUser1"
+							target="_blank"
+							data-bs-toggle="tooltip"
+							aria-label="GitHub"
+							data-bs-original-title="GitHub"><i class="fa-brands fa-github"></i></a>
+						<a
+							class="discrete-link pe-2"
+							href="https://bsky.app/profile/ciiro.dev"
+							target="_blank"
+							data-bs-toggle="tooltip"
+							aria-label="Bluesky"
+							data-bs-original-title="Bluesky"><i class="fa-brands fa-bluesky"></i>
+						</a>
+					</p>
+					<div class="content-separator"></div>
+					<p class="fs-5">
+						<i class="fa-solid fa-envelope"></i>
+						<a href="mailto:felix@ciiro.dev" class="discrete-link">felix@ciiro.dev</a>
+						<br />
+						<i class="fa-solid fa-key"></i>
+						<a href="data/Felix_Steer.asc" class="discrete-link">PGP Key</a>
+					</p>
+				</main>
+				<footer class="py-3">
+					<div class="pt-3">
+						by Felix at <a href="https://ciiro.dev">ciiro.dev</a>
+					</div>
+				</footer>
+			</div>
+		</div>
+	</div>
+	<script src="/js/bootstrap.bundle.js"></script>
+	<script>
+		const tooltipTriggerList = [].slice.call(
+			document.querySelectorAll('[data-bs-toggle="tooltip"]'),
+		);
+		const tooltipList = tooltipTriggerList.map(
+			(tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl),
+		);
+	</script>
 </body>
 
 </html>
